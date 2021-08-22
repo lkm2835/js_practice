2d4bb15638274a8c05b9a370f2ce96be5f4ca126
lkm2835 <lkm2835@gmail.com>
2021-08-22 16:30:44 +0900

    very simple login wtih localStorage

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
-let age = prompt("How old are you?")
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
 
-age = parseInt(age)
-isNaN(age)
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username"
 
-while (isNaN(age)) {
-    age = prompt("How old are you?")
-    age = parseInt(age)
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+
+    localStorage.setItem(USERNAME_KEY, loginInput.value);
+    printGreetings();
 }
 
-console.log(typeof age)
-console.log(age)
+function printGreetings() {
+    const username = localStorage.getItem(USERNAME_KEY)
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}!`;
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}
+else {
+    printGreetings();
+}
