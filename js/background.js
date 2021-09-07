b35366b6a62f2fcb50970de4701476c032eeb8c4
lkm2835 <lkm2835@gmail.com>
2021-09-07 21:55:29 +0900

    add background image

---
+++ js/background.js
@@ -0,0 +1,11 @@
+const images = ["0.jpg",
+                "1.jpg",
+                "2.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const backgroundImage = document.createElement("img");
+
+backgroundImage.src = `./images/${chosenImage}`
+
+document.body.appendChild(backgroundImage);
