3f274fd992551919278150ee174bc904f6b63cbe
lkm2835 <lkm2835@gmail.com>
2021-11-19 14:40:21 +0900

    delete to do list

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,21 @@ const toDoForm  = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList  = document.getElementById("todo-list");
 
+function deleteToDo() {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newToDo) {
     const newLi = document.createElement("li");
     const newSpan = document.createElement("span");
+    const button = document.createElement("button");
     
+    button.innerText = "X";
+    button.addEventListener("click", deleteToDo);
+
     newLi.appendChild(newSpan);
+    newLi.appendChild(button);
     newSpan.innerText = newToDo;
     
     toDoList.appendChild(newLi);
@@ -20,3 +30,4 @@ function handleToDoSubmit(event) {
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
